6757ee1a5052dd8321b890d83c295dad4eff00bc
zyxbya <92446861+zyxbya@users.noreply.github.com>
2025-12-03 09:55:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
+from collections import Counter
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
