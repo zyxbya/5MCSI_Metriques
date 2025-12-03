6f4fb93410966f1c26636d155472b896be592acf
zyxbya <92446861+zyxbya@users.noreply.github.com>
2025-12-03 09:41:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
