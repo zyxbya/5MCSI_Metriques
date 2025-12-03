dae9febc12346ae8281d660737fa9e964045ac6a
zyxbya <92446861+zyxbya@users.noreply.github.com>
2025-12-03 09:19:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,6 +26,10 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
