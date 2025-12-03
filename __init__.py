ce8bce074e69b52b3fa6cf9b84619f70ab9a55ec
zyxbya <92446861+zyxbya@users.noreply.github.com>
2025-12-03 09:16:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,18 @@ def hello_world():
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
   
 if __name__ == "__main__":
   app.run(debug=True)
