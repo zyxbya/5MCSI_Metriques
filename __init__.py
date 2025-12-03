4c354d56ab19243f1635b669700cdcd1f7f7c3ad
zyxbya <92446861+zyxbya@users.noreply.github.com>
2025-12-03 09:46:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ def hello_world():
     return render_template('hello.html') #Comm2
 
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def contact():
+    return render_template("contact.html")
 
 @app.route('/tawarano/')
 def meteo():
